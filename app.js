9467d5f8358251890d9034e89306d0821125ae50
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2023-02-15 15:04:34 -0800

    asynchronous

--- app.js
+++ app.js
@@ -10,13 +10,26 @@
 
 // server.listen(3000)
 
-
 //******Express server ********* */
-const express = require('express');
-const app = express()
+// const express = require('express');
+// const app = express()
 
-app.get('/', (req, res) => {
-  res.send("Hey whats up from express")
-})
+// app.get('/', (req, res) => {
+//   res.send("Hey whats up from express")
+// })
 
-app.listen(3000)
+// app.listen(3000)
+
+//*********fs file system********* */
+const fs = require("fs");
+const fileName = "target.txt";
+
+// fs.watch(fileName, () => console.log(`File changed!`));
+fs.readFile(fileName, (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
+});
+
+console.log("Node js async programming...?");
